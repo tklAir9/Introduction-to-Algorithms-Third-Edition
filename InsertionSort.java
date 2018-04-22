a27f2494fa19a37e3ae5aa792fe16c4de3e85ad5
Uzair Hasan <uzairhasan12@hotmail.com>
2018-04-22 10:26:34 -0400

    Update InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -1,23 +1,24 @@
-
+//Insertion Sort By Uzair Hasan
+//This was converted from pseduocode to Java
 public class InsertionSort {
 	public static void main(String[] args) {
-		int A[] = { 5, 2, 4, 6, 1, 3 };
+		int A[] = { 5, 2, 4, 6, 1, 3 }; //Random test array
 		int B[] = insertionSort(A);
-		for (int ii = 0; ii < B.length; ii++) {
-			System.out.println(B[ii]);
-		}
 	}
 
-	public static int[] insertionSort(int[] B) {
-		for (int i = 1; i < B.length; i++) {
-			int key = B[i];
-			int j = i - 1;
-			while (j >= 0 && B[j] > key) {
-				B[j + 1] = B[j];
-				j--;
+	public static int[] insertionSort(int[] B) { 
+		for (int i = 1; i < B.length; i++) { //Notice that we start from index 1, since starting at 0 would be inefficient
+			int key = B[i]; //Remember the current value we are observing
+			int j = i - 1; //Move one index back as we will be evaluating whether the value before key is larger
+			while (j >= 0 && B[j] > key) { //short circuit if we are out of index of array (-1)
+				B[j + 1] = B[j]; // shift the value that is greater than key by one to the right
+				j--; //Move the index value one back and repeat while loop
 			}
-			B[j + 1] = key;
+			B[j + 1] = key; // Since whatever was shifted right last has a duplicate in the array and the key value is missng, remove the left dublicated value with key 
 		}
 		return B;
 	}
 }
+
+//Any questions, feel free to email me and ask!
+//uzairhasan12@hotmail.com
